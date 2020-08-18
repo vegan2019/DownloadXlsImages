c857fa4f8b61bd668988cdc927fdba9e4a974593
vegan2019 <58324192+vegan2019@users.noreply.github.com>
2020-08-18 17:08:29 +0800

    Create webserver.go

---
+++ webserver.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+        "log"
+        "net/http"
+)
+
+func main() {
+        // Simple static webserver:
+        log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("./HUB-images"))))
+}
+             
